f826c2768c551f37f55a7de1fb5e04303d0adc74
Cuong, To <totucuong@zedat.fu-berlin.de>
2017-12-20 11:04:49 +0100

    Update setup

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 
 setup(name='spectrum',
       version='0.1',
-      description='A knowledge fusion library',
+      description='A python library for data fusion',
       url='https://github.com/totucuong/spectrum',
       author='To Tu Cuong',
-      author_email='totucuong@gmail.com',
+      author_email='to.cuong@fu-berlin.de',
       license='Apache-2.0',
-      packages=['spectrum'],
+      packages=['spectrum', 'spectrum.datasets', 'spectrum.inferences', 'spectrum.models'],
+      extras_require={
+          'numpy': ['numpy>=1.13.3']
+      },
       zip_safe=False)
